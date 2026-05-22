3071b1d0ec87d0f39cca8975cd6cb8eb93cc7750
Jonas <jonas.noethel@mailbox.tu-dresden.de>
2026-05-22 12:03:49 +0200

    Added more surrogate measurements

--- main_node/reconfiguration/tests/test_approach_performance.py
+++ main_node/reconfiguration/tests/test_approach_performance.py
@@ -35,20 +35,22 @@ class TestApproachPerformance:
         start = timeit.default_timer()
 
         # Change
-        desc = {"Instance": {
-                        "LinearRegression": {
-                            "MultiObjective": False,
-                            "Type": "sklearn_model_wrapper",
-                            "Class": "sklearn.linear_model.LinearRegression"
-                        }
-                    }}
-        reconf_module.change_variant("Surrogate", desc)
-        reconf_module.done().reconfigure()
+        iterations = 20
+        for i in range(iterations):
+            desc = {"Instance": {
+                            "LinearRegression": {
+                                "MultiObjective": False,
+                                "Type": "sklearn_model_wrapper",
+                                "Class": "sklearn.linear_model.LinearRegression"
+                            }
+                        }}
+            reconf_module.change_variant("Surrogate", desc)
+            reconf_module.done().reconfigure()
 
         end = timeit.default_timer()
         duration = end - start
 
-        self.save_result("surrogate", 1, duration)
+        self.save_result("surrogate", iterations, duration)
 
     def test_change_single_surrogate(self, reconf_module_multi_features_single_model:ReconfigurationModule):
         reconf_module = reconf_module_multi_features_single_model
